84e6bca4f09cce8c96c23a2ad403f6b39e85371c
t-nan <t-nan@yandex.ru>
2020-09-10 00:09:35 +0300

    add test for power_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -5,5 +5,13 @@ describe Hero do
   it "has a capitalized name" do
     hero=Hero.new 'mike'
 
-    expect(hero.name)to eq 'Mike' #hero.name='Mike'
+    expect(hero.name).to eq 'Mike' #hero.name=='Mike'
+  end
+
+  it "can power up" do
+    hero=Hero.new 'mike'
+
+    expect(hero.power_up).to eq 110
+  end
+
 end
