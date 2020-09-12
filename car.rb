6cf43caa60549197a04cd4fda56b9dedad72e1f3
t-nan <t-nan@yandex.ru>
2020-09-12 19:02:47 +0300

    add Car class

--- car.rb
+++ car.rb
@@ -1,2 +1,25 @@
 class Car
+
+  MILES_PER_GALLON = 20
+
+  attr_reader :fuel
+
+  def initialize
+    @fuel=0
+  end
+
+  def add_fuel amount
+    @fuel+=amount
+  end
+
+  # как далеко сможем проехать
+
+  def range
+    0
+  end
+
 end
+
+car=Car.new
+car.add_fuel 10
+puts "Range is #{car.range}"  # сможем проехать ... миль
